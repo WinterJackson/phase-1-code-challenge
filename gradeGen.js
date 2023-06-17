3494074c828f2863d264319afb7537de16fa26b6
WinterJackson <winterjacksonwj@gmail.com>
2023-06-17 23:16:04 +0300

    In Progress

--- gradeGen.js
+++ gradeGen.js
@@ -1,11 +1,24 @@
-const grader = document.querySelector('button');
-grader.addEventListener('click', function() {
-  const marksInput = document.querySelector('#marks');
-  const nameInput = document.querySelector('#student-name');
-  const marks = parseInt(marksInput.value);
-  const name = nameInput.value;
-  let grade = '';
+// Get the button element with the tag name 'button'
+const grader = document.querySelector('button'); 
 
+// Add event listener to the grader button and define an anonymous function to be executed when clicked
+grader.addEventListener('click', function() { 
+  // Get the input element for marks  
+  const marksInput = document.querySelector('#marks'); 
+
+  // Get the input element for the student name
+  const nameInput = document.querySelector('#student-name'); 
+
+  // Get the value of the marks input and convert it to an integer using parseInt()
+  const marks = parseInt(marksInput.value); 
+
+  // Get the value of the student name input
+  const name = nameInput.value; 
+
+  // Initialize an empty string for the grade
+  let grade = ''; 
+
+  // Determine the grade based on the marks
   if (marks >= 79 && marks <= 100) {
     grade = "A";
   } else if (marks >= 60 && marks <= 79) {
@@ -20,5 +33,6 @@ grader.addEventListener('click', function() {
     grade = "INVALID INPUT";
   }
 
-  alert(`Student: ${name}\nGrade: ${grade}`);
+  // Display the student name and grade in an alert
+  alert(`Student: ${name}\nGrade: ${grade}`); 
 });
